adb91e3c76611a25bf31b9f972955f395a6992e7
Leo Chui <z.chui1@universityofgalway.ie>
2024-01-27 19:26:38 +0000

    Add rmisample-win for reference

---
+++ rmisample-win/start-client-windows.bat
@@ -0,0 +1,1 @@
+java -cp c:\users\lchui\rmisample-win -Djava.rmi.server.codebase=file:/c:/users/lchui/rmisample-win/ -Djava.security.policy=server-windows.policy CityClient
---
+++ rmisample-win/start-registry-windows.bat
@@ -0,0 +1,1 @@
+rmiregistry -J-Djava.rmi.server.codebase=file:/c:/users/lchui/rmisample-win/\
