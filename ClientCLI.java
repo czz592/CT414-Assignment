5e0889849d80f0a32a18bf22440a245da808ef3a
Leo Chui <z.chui1@universityofgalway.ie>
2024-01-27 19:15:01 +0000

    Add name lookup code to ClientCLI

--- ClientCLI.java
+++ ClientCLI.java
@@ -1,9 +1,20 @@
+import java.rmi.*;
+
 public class ClientCLI {
     // Command Line Interface for the client
     // that calls the server methods
     // on an AssessmentImpl object
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("ClientCLI");
+        // Name lookup on the server
+        // to get the ExamEngine object
+        // and call its methods
+        try {
+            ExamEngine engine = (ExamEngine) Naming.lookup("//localhost/ExamServer");
+            // TODO - call methods on the server
+            // and passing CLI args to the server
+
+        } catch (Exception e) {
+            System.out.println("Error in main - " + e.toString());
+        }
     }
 }
